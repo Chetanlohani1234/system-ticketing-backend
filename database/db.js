9359f453c55a556753a5d333999043de9d0aa335
chetanlohani4321 <chetanlohani310@gmail.com>
2024-03-17 19:52:50 +0530

    changes

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 //const url="mongodb://127.0.0.1:27017/Ticketing"
-const url = "mongodb+srv://root:MongoDb%40123!@cluster0.8ujyaqs.mongodb.net/Ticketing"
-
+//const url = "mongodb+srv://root:MongoDb%40123!@cluster0.8ujyaqs.mongodb.net/Ticketing"
+const url = "mongodb+srv://lohanichetan4:MongoDb1234@ticketing-cluster.15tb2sf.mongodb.net/Ticketing";
 module.exports.connectdb=async(req,res)=>{
     try{ 
         await mongoose.connect(url,{
